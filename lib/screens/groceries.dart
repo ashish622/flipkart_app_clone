2887fdd78ebec5d8517b6631f6ad8bf1c3e38a69
ashish622 <ashishsingh88084@gmail.com>
2023-06-28 18:10:42 +0530

    main screen changed

--- lib/screens/groceries.dart
+++ lib/screens/groceries.dart
@@ -1028,6 +1028,108 @@ class _GroceryScreenState extends State<GroceryScreen> {
                   ),
                 ),
 
+                // Flipkart Fresh
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 5),
+                  child: Text(
+                    'Flipkart Fresh',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.black
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 420,
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    height: 400,
+                    width: double.infinity,
+                    child: GridView.count(
+                      primary: false,
+                      padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      crossAxisCount: 2,
+                      children: <Widget>[
+                        Container(
+                            height: 40,
+                            width: 50,
+                            padding: const EdgeInsets.all(8),
+                            decoration: const BoxDecoration(
+                                color: Colors.white
+                            ),
+                            child: const Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Image(image: AssetImage('assets/images/product/img_17.png')),
+                                Text('Tomatoes'),
+                                Text('Best Deals on vegetables',
+                                  style: TextStyle(color: Colors.green),),
+                              ],
+                            )
+                        ),
+                        Container(
+                            height: 40,
+                            width: 50,
+                            padding: const EdgeInsets.all(8),
+                            decoration: const BoxDecoration(
+                                color: Colors.white
+                            ),
+                            child: const Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Image(image: AssetImage('assets/images/product/img_19.png')),
+                                Text('Fresh Broccoli'),
+                                Text('100% fresh to home',
+                                  style: TextStyle(color: Colors.green),),
+                              ],
+                            )
+                        ),
+                        Container(
+                            height: 40,
+                            width: 50,
+                            padding: const EdgeInsets.all(8),
+                            decoration: const BoxDecoration(
+                                color: Colors.white
+                            ),
+                            child: const Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Image(image: AssetImage('assets/images/product/img_19.png')),
+                                Text('Fresh Broccoli'),
+                                Text('100% fresh to home',
+                                  style: TextStyle(color: Colors.green),),
+                              ],
+                            )
+                        ),
+                        Container(
+                            height: 40,
+                            width: 50,
+                            padding: const EdgeInsets.all(8),
+                            decoration: const BoxDecoration(
+                                color: Colors.white
+                            ),
+                            child: const Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Image(image: AssetImage('assets/images/product/img_17.png')),
+                                Text('Tomatoes'),
+                                Text('Best Deals on vegetables',
+                                  style: TextStyle(color: Colors.green),),
+                              ],
+                            )
+                        ),
+                      ],
+                    ),
+                  )
+                ),
                 // Featured Collection
                 Container(
                   height: 230,
